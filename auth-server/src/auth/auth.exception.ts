79a2b3bf49e5baf0774008570cee25595f4e88a0
Allen / 임기원 <l4279625@gmail.com>
2025-05-20 08:18:21 +0900

    chore: 더미 코드 삭제 및 api 테스트용 http파일 추가

--- auth-server/src/auth/auth.exception.ts
+++ auth-server/src/auth/auth.exception.ts
@@ -2,6 +2,6 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class LoginFailedException extends HttpException {
   constructor() {
-    super('아이디 또는 비밀번호가 일치하지 않습니다.', HttpStatus.BAD_REQUEST);
+    super('Invalid id or password.', HttpStatus.BAD_REQUEST);
   }
 }
